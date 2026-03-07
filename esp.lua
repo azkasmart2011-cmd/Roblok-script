b0f69fb5a1d09e0b11c9bea2726dd77ff8103c63
azkasmart2011-cmd <azkasmart2011@gmail.com>
2026-03-07 12:13:30 +0700

    Create esp.lua

---
+++ esp.lua
@@ -0,0 +1,28 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local function ESP(player)
+    if player ~= LocalPlayer then
+        if player.Character then
+            local h = Instance.new("Highlight")
+            h.Parent = player.Character
+            h.FillColor = Color3.fromRGB(255,0,0)
+            h.OutlineColor = Color3.fromRGB(255,255,255)
+            h.FillTransparency = 0.5
+        end
+
+        player.CharacterAdded:Connect(function(char)
+            local h = Instance.new("Highlight")
+            h.Parent = char
+            h.FillColor = Color3.fromRGB(255,0,0)
+            h.OutlineColor = Color3.fromRGB(255,255,255)
+            h.FillTransparency = 0.5
+        end)
+    end
+end
+
+for _,v in pairs(Players:GetPlayers()) do
+    ESP(v)
+end
+
+Players.PlayerAdded:Connect(ESP)
